80ff2c012c4e91cb97ec845743cdba5db055f1b8
sreenath <Sreenath.Kunhikrishnan@in.tesco.com>
2016-07-12 19:29:30 +0530

    crud operation

--- src/main/java/com/houseservice/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.houseservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.houseservice.model.Userdetails;
-
-public interface UserRepository extends JpaRepository<Userdetails,Integer> {
-
-}
