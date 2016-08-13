537b94d1f1ff4fcdefc6b5d998176a6dc1a22824
sreenath <Sreenath.Kunhikrishnan@in.tesco.com>
2016-08-13 16:29:11 +0530

    new ui

--- src/main/java/com/houseservice/controller/HomeController.java
+++ src/main/java/com/houseservice/controller/HomeController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.houseservice.model.Services;
-import com.houseservice.model.Student;
+
 
 
 @RestController
