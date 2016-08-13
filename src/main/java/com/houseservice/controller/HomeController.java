d34cf7cc444ecccd05706df913c1cac766ecfc2a
vijaysharmapc <vijaysharmapc@gmail.com>
2016-08-13 16:30:48 +0530

    git shit

--- src/main/java/com/houseservice/controller/HomeController.java
+++ src/main/java/com/houseservice/controller/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.houseservice.model.Services;
-import com.houseservice.model.Student;
 
 
 @RestController
