6411fa8db6f38fefa82f4388a7d4d648e6d5f3cf
vijaysharmapc <vijaysharmapc@gmail.com>
2016-08-28 13:44:41 +0530

    LOcation service and table schema with insert in scr->db query

---
+++ src/main/java/com/houseservice/model/Locality.java
@@ -0,0 +1,53 @@
+package com.houseservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "locality")
+public class Locality {
+	  @Id
+	  @GeneratedValue(strategy = GenerationType.AUTO)
+	  private long hk_LOCALITY_ID;
+	  private String hk_ZONE;
+	  private String hk_LOCALITY;
+	  private int hk_DIST_CENTER;
+	  
+	public String getHk_ZONE() {
+		return hk_ZONE;
+	}
+	public void setHk_ZONE(String hk_ZONE) {
+		this.hk_ZONE = hk_ZONE;
+	}
+	public String getHk_LOCALITY() {
+		return hk_LOCALITY;
+	}
+	public void setHk_LOCALITY(String hk_LOCALITY) {
+		this.hk_LOCALITY = hk_LOCALITY;
+	}
+	public int getHk_DIST_CENTER() {
+		return hk_DIST_CENTER;
+	}
+	public void setHk_DIST_CENTER(int hk_DIST_CENTER) {
+		this.hk_DIST_CENTER = hk_DIST_CENTER;
+	}
+	public Locality(long hk_LOCALITY_ID, String hk_ZONE, String hk_LOCALITY, int hk_DIST_CENTER) {
+		super();
+		this.hk_LOCALITY_ID = hk_LOCALITY_ID;
+		this.hk_ZONE = hk_ZONE;
+		this.hk_LOCALITY = hk_LOCALITY;
+		this.hk_DIST_CENTER = hk_DIST_CENTER;
+	}
+	public Locality() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	  
+
+	
+	
+}
